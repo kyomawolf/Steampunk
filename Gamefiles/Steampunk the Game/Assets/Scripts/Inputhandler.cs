53dd3939ffceb5933d5536b685bf546bebced35c
kyomawolf <69203043+kyomawolf@users.noreply.github.com>
2021-04-11 14:35:12 +0200

    update 11.04

--- Gamefiles/Steampunk the Game/Assets/Scripts/Inputhandler.cs
+++ Gamefiles/Steampunk the Game/Assets/Scripts/Inputhandler.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Inputhandler : MonoBehaviour
 {
+
     public float master;
     public float music;
     public float SFX;
 
-
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 }
